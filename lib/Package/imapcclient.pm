1799b07896b95029328d0585816961a855ac79d4
Tobias Ebnöther <Ar@gorn.ch>
2009-07-16 10:53:08 +0200

    Using overwrite again

--- lib/Package/imapcclient.pm
+++ lib/Package/imapcclient.pm
@@ -29,7 +29,7 @@ sub filename {
 sub make_command {
 	my $self = shift @_;
 	my $cflags = $self->cflags();
-	return qq(MACOSX_DEPLOYMENT_TARGET=10.5 EXTRACFLAGS="$cflags" make oxp);
+	return qq(MACOSX_DEPLOYMENT_TARGET=10.5 EXTRACFLAGS="$cflags" make -e oxp);
 }
 
 
