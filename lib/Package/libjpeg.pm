38dbfea70132b0480ea908d42fea7c7035c10744
Tobias Ebnöther <Ar@gorn.ch>
2009-07-16 16:39:39 +0200

    Adjusted to new version

--- lib/Package/libjpeg.pm
+++ lib/Package/libjpeg.pm
@@ -7,17 +7,13 @@ use base qw(Package);
 
 our $VERSION = '7';
 
-
-
 sub base_url {
 	return "http://www.ijg.org/files";
 }
 
-
 sub packagename {
 	return "jpeg-$VERSION";
 }
-
 
 sub filename {
 	return "jpegsrc.v$VERSION.tar.gz";
@@ -25,44 +21,25 @@ sub filename {
 
 #http://www.ijg.org/files/jpegsrc.v7.tar.gz
 
-
-# sub build {
-# 
-# 	my $self = shift @_;
-# 	return undef unless ($self->SUPER::build(@_));
-# 	my $prefix = $self->config()->prefix();
-# 	$self->cd_packagesrcdir();
-# 	$self->shell(qq!CFLAGS="-arch i386" ./configure ! . $self->configure_flags());
-# 	$self->shell($self->make_command());
-# 	
-# }
-
-
-
 sub is_built {
 	my $self = shift @_;
 	return -e $self->packagesrcdir() . "/libjpeg.a";
 }
-
 
 sub subpath_for_check {
 	my $self = shift @_;
 	return "lib/libjpeg.a";
 }
 
-
-
 sub install {
 
 	my $self = shift @_;
 	return undef unless ($self->SUPER::install(@_));
-	
+
 	$self->cd_packagesrcdir();
-	$self->shell("make install-lib");
-	$self->shell("make install-headers");
-
+	$self->shell("make install-libLTLIBRARIES");
+	$self->shell("make install-includeHEADERS");
 }
-
 
 sub php_extension_configure_flags {
 
@@ -72,8 +49,5 @@ sub php_extension_configure_flags {
 	return "--with-jpeg-dir=" . $self->config()->prefix();
 
 }
-
-
-
 
 1;
