39a751cdba90680a957f9d09e1e1aac47dae442e
Tobias Ebnöther <Ar@gorn.ch>
2009-07-28 15:37:24 +0200

    Do not delete PHP by default

--- build-php.sh
+++ build-php.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rm -rf /tmp/build-entropy-php-pkg/php5/ /tmp/build-entropy-php-pkgdst/entropy-php.pkg src/php-5.*/ /usr/local/php5/libphp5.so
+rm -rf /tmp/build-entropy-php-pkg/php5/ /tmp/build-entropy-php-pkgdst/entropy-php.pkg
 nice -n 19 perl5.10 -Ilib build-php.pl
