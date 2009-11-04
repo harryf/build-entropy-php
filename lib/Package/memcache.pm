e5362256de8a4198503bfd0ec74bc8acff5c380a
Ivan Jovanovic <ivan@IvanBookPro.local>
2009-11-04 13:31:47 +0100

    Adding memcache patch and changing version to 3.0.4

--- lib/Package/memcache.pm
+++ lib/Package/memcache.pm
@@ -5,13 +5,19 @@ use warnings;
 
 use base qw(Package::peclbase);
 
-our $VERSION = '2.2.5';
+our $VERSION = '3.0.4';
 
 sub init {
 	my $self = shift;
 	$self->SUPER::init(@_);
 	$self->{PACKAGE_NAME} = 'memcache';
 	$self->{VERSION} = $VERSION;
+}
+
+sub patchfiles {
+	my $self = shift @_;
+
+	return qw(memcache.patch);
 }
 
 return 1;
