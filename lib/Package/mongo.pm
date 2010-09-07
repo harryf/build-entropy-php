772652aa7daf76f3240def96fcc429e6e1646799
Patrick Jezek <patrick.jezek@liip.ch>
2010-09-07 23:43:37 +0200

    adding mongo db support

---
+++ lib/Package/mongo.pm
@@ -0,0 +1,22 @@
+package Package::mongo;
+
+use strict;
+use warnings;
+
+use base qw(Package::peclbase);
+
+our $VERSION = '1.0.7';
+
+sub init {
+    my $self = shift;
+    $self->SUPER::init(@_);
+    $self->{PACKAGE_NAME} = 'mongo';
+    $self->{VERSION} = $VERSION;
+}
+
+sub packagesrcdir {
+    my $self = shift @_;
+    return $self->config()->srcdir() . "/" . $self->packagename() . "/"; 
+}
+
+return 1;
