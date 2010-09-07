070ee585f5037e5fca6fc29adc21a809083204e6
Patrick Jezek <patrick.jezek@liip.ch>
2010-09-07 23:44:53 +0200

    adding support for xslcache (by Ivan)

---
+++ lib/Package/xslcache.pm
@@ -0,0 +1,22 @@
+package Package::xslcache;
+
+use strict;
+use warnings;
+
+use base qw(Package::peclbase);
+
+our $VERSION = '0.7.1';
+
+sub init {
+	my $self = shift;
+	$self->SUPER::init(@_);
+	$self->{PACKAGE_NAME} = 'xslcache';
+	$self->{VERSION} = $VERSION;
+}
+
+sub extension_ini{
+	my ($self, $dst) = @_;
+	$self->shell({silent => 0}, "echo ';extension=" . $dst . lc($self->shortname()) . ".so' > /tmp/50-extension-" . $self->shortname() . ".ini");
+}
+
+return 1;
