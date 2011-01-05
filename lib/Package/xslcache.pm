a2222c8d04a67528dc836d7c10d2f767eb5961ed
Christian Stocker <me@chregu.tv>
2011-01-05 11:24:12 +0100

    xslcache: make sure it takes the correct xslt-config

--- lib/Package/xslcache.pm
+++ lib/Package/xslcache.pm
@@ -19,4 +19,10 @@ sub extension_ini{
 	$self->shell({silent => 0}, "echo ';extension=" . $dst . lc($self->shortname()) . ".so' > /tmp/50-extension-" . $self->shortname() . ".ini");
 }
 
+sub configure_flags {
+	my $self = shift @_;
+	my (%args) = @_;
+	return "--with-xslcache=shared," . $self->config()->prefix();
+}
+
 return 1;
